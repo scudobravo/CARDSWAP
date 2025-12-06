aef6ecfba823a9853216542aa164973f52dad028
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2025-12-06 18:27:45 +0100

    Aggiornata visualizzazione rarity nella griglia carte per usare getDisplayRarity

--- resources/js/components/listing/ChainedFilters.vue
+++ resources/js/components/listing/ChainedFilters.vue
@@ -329,7 +329,7 @@
           <!-- Rarity e Brand -->
           <div class="flex justify-between items-center text-xs">
             <span class="px-2 py-1 bg-gray-100 text-gray-700 rounded-full">
-              {{ card.rarity || 'N/A' }}{{ card.rarity_variation ? ` (${card.rarity_variation})` : '' }}
+              {{ getDisplayRarity(card) || 'N/A' }}
             </span>
             <span v-if="card.card_set?.brand" class="text-gray-500">
               {{ card.card_set.brand }}
