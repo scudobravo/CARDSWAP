107040e3878ef3dfcb9006bb4cff0c8f5372a460
Andrea Bianchi (agap2) <scudobravo@hotmail.com>
2026-01-09 12:48:53 +0100

    Fix: Aggiunto import mancante Order model in ShippoController

--- app/Http/Controllers/Api/ShippoController.php
+++ app/Http/Controllers/Api/ShippoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use App\Services\ShippoService;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
